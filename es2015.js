2e1d1149e990571dd6d447deb0b0390a2c6d42c3
kasselTrankos <alvaro.touzon@gmail.com>
2017-07-04 12:08:13 +0200

    usando es2016 para obtener el this, scope de un arrow function

--- es2015.js
+++ es2015.js
@@ -1,6 +1,12 @@
 var fs = require("fs");
 var browserify = require("browserify");
-browserify("lib/index.js")
-  .transform("babelify", {presets: ["es2015"]})
+browserify({
+	sourceType: 'module',
+	entries: ["lib/index.js"],
+	debug: true
+}).transform("babelify", {
+  	presets: ["es2016"],
+  	plugins: ['transform-object-rest-spread']
+  })
   .bundle()
   .pipe(fs.createWriteStream("app/js/bundle.js"));
