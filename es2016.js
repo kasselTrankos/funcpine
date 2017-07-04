0490eaedcb73b276d943d35082d7edc2d54debaf
vera <alvaro.touzon@gmail.com>
2017-07-04 19:42:03 +0200

    added watchify

--- es2016.js
+++ es2016.js
@@ -1,12 +1,20 @@
-var fs = require("fs");
+var fs = require("fs"),
+	watchify = require('watchify');
 var browserify = require("browserify");
-browserify({
+var b = browserify({
 	sourceType: 'module',
+	plugin: [watchify],
+	cache: true,
 	entries: ["lib/index.js"],
 	debug: true
 }).transform("babelify", {
   	presets: ["es2016"],
   	plugins: ['transform-object-rest-spread']
+  });
+
+  b.bundle()
+  .on('update', function(){
+  	console.log('File updated!!!!');
+  	b.bundle().pipe(fs.createWriteStream("app/js/bundle.js"));
   })
-  .bundle()
   .pipe(fs.createWriteStream("app/js/bundle.js"));
