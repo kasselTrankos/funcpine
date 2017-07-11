1032c8df6bb34f2219ece3e3a502daab6cff7113
kasselTrankos <alvaro.touzon@gmail.com>
2017-07-11 14:57:54 +0200

    1 - test method

--- tests/pine.spec.js
+++ tests/pine.spec.js
@@ -1,6 +1,25 @@
-import {MapNodeFound} from './../lib/pine';
-describe('app all', function(){
-	it('sumna', function(){
-		expect(1+1).toBe(2);
-	})
+import {patternToString} from './../lib/pine';
+describe('method patternToString', ()=>{
+	it('Given a pattern with valid format', ()=>{
+		let valid = [{
+			str: '0',
+			type: 'Array'
+		},{
+			str:'model',
+			type: 'Object'
+		}];
+
+		expect(patternToString(valid)).toBe('[0].model');
+	});
+	it('Given a pattern  with a bad format', ()=>{
+		let valid = [{
+			stasr: '0',
+			typess: 'Array'
+		},{
+			str:'model',
+			type: 'Object'
+		}];
+		expect(patternToString(valid)).toBe('');
+	});
+
 });
